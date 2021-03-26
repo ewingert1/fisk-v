db123fe0b4266998899c63b19eb1ccac45cc49c9
wem <wem_ctf@gmail.com>
2021-03-26 23:53:19 +0100

    ajout des define pour les timers

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 
 TOOLCHAIN_PREFIX="/home/wem/bl602_sdk/bl_iot_sdk/toolchain/riscv/Linux/bin/riscv64-unknown-elf"
 
-riscv64-elf-gcc -L /usr/riscv32-elf/lib -march=rv32i -mabi=ilp32 -nostartfiles -T linker/bl602/memmap.ld startup/bl602/* -o firmware.elf
-#riscv64-elf-as -march=rv32i -mabi=ilp32 startup/bl602/startup.S -o firmware.elf
+riscv64-elf-gcc -L /usr/riscv32-elf/lib -I/usr/riscv32-elf/include/ -march=rv32i -mabi=ilp32 -nostartfiles -T linker/bl602/memmap.ld startup/bl602/* -o firmware.elf
 riscv32-elf-objcopy -O binary firmware.elf firmware.bin
 
 #riscv64-elf-as -march=rv32i -mabi=ilp32 test.S -o firmware.elf
--- startup/bl602/bl602.c
+++ startup/bl602/bl602.c
@@ -1,16 +1,76 @@
+#include <stdint.h>
+
+#define GPIO_BASE 	0x40000100
+#define GPIO_OUTPUTCFG	0b0000101100000000
+#define GPIO_INPUTCFG	0b0000101100000000
+
+#define GPIO_GPIO_ENABLE_OFFSET 0x90
+#define GPIO_GPIO_OUTPUT_OFFSET 0x88
+
+
+
+
+#define TIMER_BASE 		0x4000a500
+#define TCCR_REG		TIMER_BASE
+#define TMR2_0_REG		TIMER_BASE + 0x10
+#define TMR2_1_REG		TIMER_BASE + 0x14
+#define TMR2_2_REG		TIMER_BASE + 0x18
+#define TMR3_0_REG		TIMER_BASE + 0x1C
+#define TMR3_1_REG		TIMER_BASE + 0x20
+#define TMR3_2_REG		TIMER_BASE + 0x24
+
+#define TCR2_REG		TIMER_BASE + 0x2C
+#define TCR3_REG		TIMER_BASE + 0x30
+#define TMSR2_REG		TIMER_BASE + 0x34
+#define TMSR3_REG		TIMER_BASE + 0x38
+
+#define TIER2_REG		TIMER_BASE + 0x44
+#define TIER3_REG		TIMER_BASE + 0x48
+
+#define TICR2_REG		TIMER_BASE + 0x78
+#define TICR3_REG		TIMER_BASE + 0x7C
+
+#define TCER_REG		TIMER_BASE + 0x84
+#define TCMR_REG		TIMER_BASE + 0x88
+
 
 void bl602_irq_handler()
 {
 
+
+	*((uint32_t*)TICR2_REG) = 1;
+}
+
+void gpio11_toggle()
+{
+	*((uint32_t*)(GPIO_BASE+0x88)) = *((uint32_t*)(GPIO_BASE+0x88)) ^ (1 << 11);
+}
+
+void gpio11_init()
+{
+	uint32_t gpio11_cfg = 0b00001011000000000000000000000000;
+
+	/* gpio mode on gpio11 */
+	*((uint32_t*)(GPIO_BASE+0x14)) = gpio11_cfg;
+	/* output enable on gpio11 */
+	*((uint32_t*)(GPIO_BASE+0x90)) = 1 << 11;
+}
+
+void timer_init()
+{
+	/* clock 32k pour timer2 */
+	*((uint32_t*)TCCR_REG) |= 0b0100;
+	/* match with 32000 => T=1s a peu pres */
+	*((uint32_t*)TMR2_0_REG) = 32000;
+	/* enable interrupt on timer2 with match register 0 */
+	*((uint32_t*)TIER2_REG) = 1;
+	/* count enable pour timer2 */
+	*((uint32_t*)TCER_REG) = 2;
+	/* free run pour timer2 => ca compte de 0 à match reg0 => interrupt */
+	*((uint32_t*)TCMR_REG) = 2;
 }
 
 void bl602_main()
 {
-	unsigned int gpio_base = 0x40000100;
-	unsigned int gpio11_cfg = 0b00001011001000000000000000000000;
-	unsigned int gpio11_bit = 0b00000000000000000000100000000000;
 
-	*((unsigned int*)(gpio_base+0x14)) = gpio11_cfg;
-	*((unsigned int*)(gpio_base+0x90)) = gpio11_bit;
-	// *((unsigned int*)(gpio_base+0x88)) = ~gpio11_bit;
 }
